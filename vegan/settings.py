76d2b679544dade31aa45e4f528e54f2f85cd537
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2017-02-04 12:40:12 -0800

    set up postgis

--- vegan/settings.py
+++ vegan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,12 @@ WSGI_APPLICATION = 'vegan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'restaurant_db',
     }
 }
 
+POSTGIS_VERSION = (2, 2, 2)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
